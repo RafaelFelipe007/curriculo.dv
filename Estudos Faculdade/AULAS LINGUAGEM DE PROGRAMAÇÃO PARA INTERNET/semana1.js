be7d19c1864e1a28ab4f94c8b190a6ac3b0a15fc
RAFAEL.FELIPE <rafaelfelipeoliveiras2@gmail.com>
2025-08-27 20:09:59 -0300

    Atualiza a função ExibirTextoPersonalizado para incluir "Mundo" na saudação e corrige a formatação do JSX.

--- Estudos Faculdade/AULAS LINGUAGEM DE PROGRAMAÇÃO PARA INTERNET/semana1.js
+++ Estudos Faculdade/AULAS LINGUAGEM DE PROGRAMAÇÃO PARA INTERNET/semana1.js
@@ -1,8 +1,12 @@
 function ExibirTextoPersonalizado(props) {
-return <h1>Olá, {props.name} {props.idade}</h1>
+  return (
+    <h1>
+      Olá Mundo, {props.name} {props.idade}
+    </h1>
+  );
 }
 
 const Home = () => {
-<ExibirTextoPersonalizado nome="Rafael" idade="23"/>
-}
-export default Home
+  <ExibirTextoPersonalizado nome="Rafael" idade="23" />;
+};
+export default Home;
